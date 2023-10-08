93630d6ce45f2396973fa11b98fe4fd019965bad
Benthan Vu <bxvu@ucsc.edu>
2023-10-07 23:58:26 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,18 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let num: number = 0;
+const count = document.createElement("div");
+count.innerHTML = `Number of 🦐 Interactions : ${num}`;
+app.append(count);
+
 const button = document.createElement("button");
 button.innerHTML = "Shromp 🦐";
 app.append(button);
+
+button.addEventListener("click", () => {
+  button.innerHTML =
+    button.innerHTML === "Shromp 🦐" ? "Shrompled 🦐" : "Shromp 🦐";
+  num++;
+  count.innerHTML = `Number of 🦐 Interaction : ${num}`;
+});
