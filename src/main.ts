a58006b06c4188a5e4ce688a5697f0e90422d440
Benthan Vu <bxvu@ucsc.edu>
2023-10-11 11:36:25 -0700

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,25 @@ app.append(header);
 
 let num: number = 0;
 const count = document.createElement("div");
-// count.innerHTML = `Number of 🐚 : ${num}`;
 app.append(count);
+
+const autoclickerDisplay = document.createElement("div");
+app.append(autoclickerDisplay);
+
+//manual clicking
+const button = document.createElement("button");
+button.innerHTML = "Shromp 🦐";
+app.append(button);
+
+button.addEventListener("click", () => {
+  button.innerHTML =
+    button.innerHTML === "Shromp 🦐" ? "Shrompled 🦐" : "Shromp 🦐";
+  num++;
+  updateUI();
+});
+
+const growthRate = document.createElement("div");
+app.append(growthRate);
 
 interface autoclickers {
   cost: number;
@@ -21,6 +38,7 @@ interface autoclickers {
   amount: number;
 }
 
+// crab
 class crab implements autoclickers {
   cost: number;
   units: number;
@@ -32,28 +50,14 @@ class crab implements autoclickers {
     this.units = 0.1;
     this.amount = 0;
     this.disabled = true;
-    this.name = "Crab";
+    this.name = "Crab Helper 🦀";
   }
 }
 
 const crabData = new crab();
-const growthRate = document.createElement("div");
-// growthRate.innerHTML = `Number of 🦀 : ${crabData.amount}`;
-app.append(growthRate);
-
-const button = document.createElement("button");
-button.innerHTML = "Shromp 🦐";
-app.append(button);
-
-button.addEventListener("click", () => {
-  button.innerHTML =
-    button.innerHTML === "Shromp 🦐" ? "Shrompled 🦐" : "Shromp 🦐";
-  num++;
-  count.innerHTML = `Number of 🐚 : ${num}`;
-});
 
 const crabHelper = document.createElement("button");
-crabHelper.innerHTML = "Crab Helper 🦀";
+crabHelper.innerHTML = crabData.name;
 app.append(crabHelper);
 
 crabHelper.addEventListener("click", () => {
@@ -62,6 +66,68 @@ crabHelper.addEventListener("click", () => {
   }
   crabData.amount++;
   num -= crabData.cost;
+  updateUI();
+});
+
+//item 2
+class crab2 implements autoclickers {
+  cost: number;
+  units: number;
+  amount: number;
+  disabled: boolean;
+  name: string;
+  constructor() {
+    this.cost = 100;
+    this.units = 2;
+    this.amount = 0;
+    this.disabled = true;
+    this.name = "Crab Helper 2🦀";
+  }
+}
+
+const crabData2 = new crab2();
+
+const crabHelper2 = document.createElement("button");
+crabHelper2.innerHTML = crabData2.name;
+app.append(crabHelper2);
+
+crabHelper2.addEventListener("click", () => {
+  if (num < crabData2.cost) {
+    return;
+  }
+  crabData2.amount++;
+  num -= crabData2.cost;
+  updateUI();
+});
+
+//item 3
+class crab3 implements autoclickers {
+  cost: number;
+  units: number;
+  amount: number;
+  disabled: boolean;
+  name: string;
+  constructor() {
+    this.cost = 1000;
+    this.units = 50;
+    this.amount = 0;
+    this.disabled = true;
+    this.name = "Crab Helper 3🦀";
+  }
+}
+
+const crabData3 = new crab3();
+
+const crabHelper3 = document.createElement("button");
+crabHelper3.innerHTML = crabData3.name;
+app.append(crabHelper3);
+
+crabHelper3.addEventListener("click", () => {
+  if (num < crabData3.cost) {
+    return;
+  }
+  crabData3.amount++;
+  num -= crabData3.cost;
   updateUI();
 });
 
@@ -75,29 +141,47 @@ function step() {
 
   if (elapsed > 1000) {
     start = undefined;
-    if (crabData.amount > 0) {
-      button.innerHTML =
-        button.innerHTML === "AutoShromp 🦐"
-          ? "AutoShromped 🦐"
-          : "AutoShromp 🦐";
-      num += crabData.units;
+    if (crabData.amount || crabData2.amount || crabData3.amount) {
+      num += calculateGrowthRate();
       updateUI();
     }
   }
 
-  if (!crabHelper.disabled && num < 10) {
+  if (!crabHelper.disabled && num < crabData.cost) {
     crabHelper.disabled = true;
-  } else if (crabHelper.disabled && num >= 10) {
+  } else if (crabHelper.disabled && num >= crabData.cost) {
     crabHelper.disabled = false;
+  }
+
+  if (!crabHelper2.disabled && num < crabData2.cost) {
+    crabHelper2.disabled = true;
+  } else if (crabHelper2.disabled && num >= crabData2.cost) {
+    crabHelper2.disabled = false;
+  }
+
+  if (!crabHelper3.disabled && num < crabData3.cost) {
+    crabHelper3.disabled = true;
+  } else if (crabHelper3.disabled && num >= crabData3.cost) {
+    crabHelper3.disabled = false;
   }
 
   window.requestAnimationFrame(step);
 }
 
+updateUI();
 window.requestAnimationFrame(step);
 
 function updateUI() {
-  // format number to 2 decimal places
+  // format number to 1 decimal place
   count.innerHTML = `Number of 🐚 : ${num.toFixed(1)}`;
-  growthRate.innerHTML = `Growth Rate: ${crabData.amount}`;
+  growthRate.innerHTML = `Growth Rate: ${calculateGrowthRate().toFixed(1)}/sec`;
+  autoclickerDisplay.innerHTML = `Crab Helper 1: ${crabData.amount} | Crab Helper 2: ${crabData2.amount} | Crab Helper 3: ${crabData3.amount}`;
+}
+
+function calculateGrowthRate() {
+  return (
+    crabData.amount * crabData.units +
+    crabData2.amount * crabData2.units +
+    crabData3.amount * crabData3.units
+  );
 }
